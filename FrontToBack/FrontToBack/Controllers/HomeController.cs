d90a8300cafacb15f075356b8baad1e8d00cf193
Your Name <you@example.com>
2023-11-17 09:20:15 +0400

    dinamic product

--- FrontToBack/FrontToBack/Controllers/HomeController.cs
+++ FrontToBack/FrontToBack/Controllers/HomeController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FrontToBack.DAL;
+using FrontToBack.Models;
+using FrontToBack.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FrontToBack.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+                
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Slide> slides = _context.Slides.ToList();
+            List<Product> products = _context.Products.ToList();
+            List<Product> lastproducts = _context.Products.OrderByDescending(x=>x.Id).Take(8).ToList();
+
+
+            HomeVM home = new HomeVM
+            {
+                Products = products,
+                Slides = slides,
+                LastestProducts = lastproducts,
+            };
+
+            return View(home);
         }
         public IActionResult About()
         {
---
+++ FrontToBack/FrontToBack/ViewModels/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using FrontToBack.Models;
+
+namespace FrontToBack.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Slide>  Slides { get; set; }
+        public List<Product> Products { get; set; }
+        public List<Product> LastestProducts { get; set; }
+
+    }
+}
